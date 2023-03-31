fe953b3a396d1611f3eeae2c1af630f36e1c57ee
João <joaovictorpfr@gmail.com>
2023-03-31 14:12:02 -0300

    Add stickers for arguments in class

--- src/classes/stickers/Stickers.java
+++ src/classes/stickers/Stickers.java
@@ -6,21 +6,33 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Stickers {
-    public void create() throws IOException {
+    public void create(InputStream inputStream, String fileName) throws IOException {
 
         // lendo a imagem:
-        BufferedImage image = ImageIO.read(new File(""));
+        BufferedImage image = ImageIO.read(new File(inputStream.toString()));
 
         // cria imagem em memória com transparência e com tamanho novo
-        int largura = image.getWidth();
-        int alturaOr = image.getHeight();
-        int altura = alturaOr + 200;
-        BufferedImage novaImagem = new BufferedImage(largura, altura, BufferedImage.TRANSLUCENT);
+        int width = image.getWidth();
+        int oldHeight = image.getHeight();
+        int height = oldHeight + 200;
+        BufferedImage novaImagem = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
 
         // copiar a imagem original para novo imagem (em memória);
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(image, 0, 0, null);
+
+        // configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 100, height - 100);
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(fileName));
     }
 }
